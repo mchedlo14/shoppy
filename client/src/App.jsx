e58036b4f184c20a53263009b43d4bd407514112
mchedlo14 <golocogo4@gmail.com>
2023-05-22 12:09:51 +0400

    added router

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,34 +1,23 @@
 import React from "react";
-import data from "./data";
 import "./App.css";
+import { BrowserRouter, Routes, Route,Link } from "react-router-dom";
+import HomeScreen from "./screens/HomeScreen";
+import ProductScreen from "./screens/ProductScreen";
 const App = () => {
   return (
-    <div>
-      <header>
-        <a href="/">shoppy</a>
-      </header>
-      <main>
-        <h1>Featured Products</h1>
-        <div className="products">
-          {data.products.map((product) => (
-            <div key={product.slug} className="product">
-              <a href={`/product/${product.slug}`}>
-                <img src={product.image} alt={product.name} />
-              </a>
-              <div className="product-info">
-                <a href={`/product/${product.slug}`}>
-                  <p>{product.name}</p>
-                </a>
-                <p>
-                  <strong>${product.price}</strong>
-                </p>
-                <button>Add to cart</button>
-              </div>
-            </div>
-          ))}
-        </div>
-      </main>
-    </div>
+    <BrowserRouter>
+      <div>
+        <header>
+          <Link to="/">shoppy</Link>
+        </header>
+        <main>
+          <Routes>
+            <Route path="/product/:slug" element={<ProductScreen />}/>
+            <Route path="/" element={<HomeScreen />}/>
+          </Routes>
+        </main>
+      </div>
+    </BrowserRouter>
   );
 };
 
