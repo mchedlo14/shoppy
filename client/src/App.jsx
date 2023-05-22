24421bc36b8a0de7fdb6564f5510da5884cd2600
mchedlo14 <golocogo4@gmail.com>
2023-05-22 11:52:18 +0400

    added product list

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,35 @@
-import React from 'react'
-
+import React from "react";
+import data from "./data";
+import "./App.css";
 const App = () => {
   return (
     <div>
       <header>
-        <a href='/'>shoppy</a>
+        <a href="/">shoppy</a>
       </header>
       <main>
-        list products
+        <h1>Featured Products</h1>
+        <div className="products">
+          {data.products.map((product) => (
+            <div key={product.slug} className="product">
+              <a href={`/product/${product.slug}`}>
+                <img src={product.image} alt={product.name} />
+              </a>
+              <div className="product-info">
+                <a href={`/product/${product.slug}`}>
+                  <p>{product.name}</p>
+                </a>
+                <p>
+                  <strong>${product.price}</strong>
+                </p>
+                <button>Add to cart</button>
+              </div>
+            </div>
+          ))}
+        </div>
       </main>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
---
+++ client/src/data.js
@@ -0,0 +1,55 @@
+const data = {
+  products: [
+    {
+      name: "Nike Slim shirt",
+      slug: "Nike-slim-shirt",
+      category: "Shirts",
+      image: "/images/p1.jpg",
+      price: 120,
+      countInStock: 10,
+      brand: "Nike",
+      rating: 4.5,
+      numReviews: 10,
+      description: "hign quality shorts",
+    },
+    {
+      name: "Adidas Fit shirt",
+      slug: "Adidas-fit-shirt",
+      category: "Shirts",
+      image: "/images/p2.jpg",
+      price: 250,
+      countInStock: 20,
+      brand: "Adidas",
+      rating: 4.0,
+      numReviews: 10,
+      description: "hign quality product",
+    },
+    {
+      name: "Nike Slim Pant",
+      slug: "Nike-slim-pant",
+      category: "Pants",
+      image: "/images/p3.jpg",
+      price: 25,
+      countInStock: 15,
+      brand: "Nike",
+      rating: 4.5,
+      numReviews: 14,
+      description: "hign quality product",
+    },
+    {
+      name: "Adidas Fit pant",
+      slug: "Adidas-fit-pant",
+      category: "Pants",
+      image: "/images/p4.jpg",
+      price: 65,
+      countInStock: 5,
+      brand: "Adidas",
+      rating: 4.5,
+      numReviews: 10,
+      description: "hign quality product",
+    },
+  ],
+};
+
+
+export default data;
