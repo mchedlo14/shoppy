f0c5afc68d8dd4447bb499bc4dc84eb484f5d4c6
mchedlo14 <golocogo4@gmail.com>
2023-05-22 11:11:51 +0400

    initial commit

---
+++ client/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <header>
+        <a href='/'>shoppy</a>
+      </header>
+      <main>
+        list products
+      </main>
+    </div>
+  )
+}
+
+export default App
